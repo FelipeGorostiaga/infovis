a3edac1c8e8503b3cbac2dcab73f26a88b974eda
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-04-15 17:04:20 -0300

    Added authorization to spotify api

--- personal-data/streaming.py
+++ personal-data/streaming.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import requests
+import base64
+import json
+
+# TODO: get from .env file
+spotify_base_address =  'https://api.spotify.com.'
+client_id =  '977f73bb16a84406b785e8fd3d66be02'
+client_secret = 'd8100158832a47079e45c9b91a91fbb0' 
 
 # Streaming History 
-df_s0 = pd.read_json('./data/StreamingHistory0.json')
+""" df_s0 = pd.read_json('./data/StreamingHistory0.json')
 df_s1 = pd.read_json('./data/StreamingHistory1.json')
 df_s2 = pd.read_json('./data/StreamingHistory2.json')
 
@@ -17,4 +25,22 @@ print(f'Track count: {track_count}')
 print(f'Most listened track: {top_tracks}')
 print(f'Most listened artist: {top_artists}')
 
-df.to_csv('streaming.csv', index = False)
+df.to_csv('streaming.csv', index = False) """
+
+client_credentials = f'{client_id}:{client_secret}'
+creds_64 = base64.b64encode(client_credentials.encode())
+auth_url = 'https://accounts.spotify.com/api/token'
+auth_method = 'POST'
+token_data = {
+    'grant_type': 'client_credentials'
+}
+token_headers = {
+    'Authorization': f'Basic {creds_64.decode()}' 
+}
+# Make request to Spotify API to get track genre
+
+r = requests.post(auth_url, data=token_data, headers=token_headers)
+response_data = r.json()
+access_token = response_data['access_token']
+expires_in = response_data['expires_in']
+print(access_token)
